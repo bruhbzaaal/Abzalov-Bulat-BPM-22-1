6b5c4b27bf7eacfa0960e126fac47441aec52358
bruhbzaaal <113934002+bruhbzaaal@users.noreply.github.com>
2022-10-03 21:18:42 +0300

    Update hw-05-01.cpp

--- hw-05-01.cpp
+++ hw-05-01.cpp
@@ -7,23 +7,34 @@ int main() {
 	double bet = 0.1;
 	double x = a;
 	const double eps = 0.001;
-//	std::cout << " x" << " " << "|" << " " << "s(x)" << " " << "|" << " " << "f(x)" << std::endl;
+	std::cout << " x" << " " << "|" << " " << "s(x)" << " " << "|" << " " << "f(x)" << std::endl;
 	double s = 0.0;
 	double y;
 	double s_i = 1.0;
 	int i = 0;
 	int fact = 1; // для вычисления факториала в знаменателе
-	while (abs(s_i) > eps && x <= b) {
-		if (i != 0) {
-			for (int j = 1; j <= i * 2; ++j) {
-				fact = fact * j;
+	while (x <= b) {
+		while (abs(s_i) > eps) {
+			if (i != 0) {
+				for (int j = 1; j <= i * 2; ++j) {
+					fact = fact * j;
+				}
 			}
+			s_i = (pow(-1, i) * pow(x, 2 * i)) / fact;
+			y = cos(x);
+			s = s + s_i;
+			i += 1;
+			fact = 1;
 		}
-		s_i = (pow(-1, i) * pow(x, 2 * i)) / fact;
-		y = cos(x);
-		s = s + s_i;
-		std::cout << " " << x << " " << "|" << " " << s << " " << "|" << " " << y << std::endl;
+		if (x > -0.05 && x < 0.05) {
+			std::cout << " " << 0 << " " << "|" << " " << s << " " << "|" << " " << y << std::endl;
+		}
+		else {
+			std::cout << " " << x << " " << "|" << " " << s << " " << "|" << " " << y << std::endl;
+		}
 		x = x + bet;
-		i += 1;
+		s = 0.0;
+		s_i = 1.0;
+		i = 0;
 	}
 }
