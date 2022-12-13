6cc03fba0a73446d96b06a58b7ee9b7a333a7550
bruhbzaaal <113934002+bruhbzaaal@users.noreply.github.com>
2022-12-13 15:39:27 +0300

    Update Geom.cpp

--- abzalov_b_v/geom_library/Geom.cpp
+++ abzalov_b_v/geom_library/Geom.cpp
@@ -1,8 +1,6 @@
 #include "Geom.hpp"
 #include <iostream>
 
-
-
 std::ostream& operator<<(
 	std::ostream& ostrm,
 	const Rdec2D& p
@@ -10,9 +8,29 @@ std::ostream& operator<<(
 	return ostrm << '(' << p.x << ',' << p.y << ')';
 }
 
+std::ostream& operator<<(std::ostream& out, const Rpol2D& point) {
+	out << "(" << point.r << "," << point.phi << ")" << "\n";
+	return out;
+}
+
+Rdec2D ToDec(Rpol2D vector) {
+	double x = std::cos(vector.phi) * vector.r;
+	double y = std::sin(vector.phi) * vector.r;
+	Rdec2D new_vector{ x, y };
+	return new_vector;
+}
+
+Rpol2D ToPol(Rdec2D vector) {
+	double r = std::sqrt(vector.x * vector.x + vector.y * vector.y);
+	double phi = std::asin(vector.y / r);
+	Rpol2D new_vector{ r, phi };
+	return new_vector;
+}
+
 Rdec2D operator+=(Rdec2D& lhs, const Rdec2D& rhs) {
 	lhs.x += rhs.x;
 	lhs.y += rhs.y;
+
 	return lhs;
 }
 
@@ -98,10 +116,3 @@ double norm(const Rdec2D& lhs) {
 	double res = sqrt(cop.x + cop.y);
 	return res;
 }
-
-
-
-
-
-
-
